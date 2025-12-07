85885f7b50eab1ce8a7790d0a4abc036a5045aba
Hossain-Shifat <161940464+hossain-shifat@users.noreply.github.com>
2025-12-07 08:39:55 +0600

    Add initial server code

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const cors = require('cors');
+const app = express()
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const port = process.env.PORT || 3000
+
+
+// middlewere
+app.use(express.json())
+app.use(cors())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pbqwzvg.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+
+async function run() {
+    try {
+        await client.connect();
+
+        // collections
+        const db = client.db('Micro_Loan')
+        
+    }
+    finally{
+
+    }
+}
